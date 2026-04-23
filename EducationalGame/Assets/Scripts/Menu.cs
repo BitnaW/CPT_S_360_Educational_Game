d78e01d9bf6753ffc8f1fc6133918b0bd65f7a2a
BitnaW <bitna.white@wsu.edu>
2026-04-22 23:50:27 -0700

    Main menu + Options + Level Selector Screen

--- EducationalGame/Assets/Scripts/Menu.cs
+++ EducationalGame/Assets/Scripts/Menu.cs
@@ -1,19 +1,37 @@
 using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    // menu coordinator that handles panel switching, scene loading
+    [SerializeField] private MenuStateCoordinator menuStateCoordinator;
+
+    // called by the play button, opens the stage select panel
     public void onPlayButton()
     {
-        // the (1) is the order of levels/scenes
-        // make sure they're in the right order in the build settings 
-        // menu should be 0
-        SceneManager.LoadScene(1);
+        menuStateCoordinator.ShowStageSelect();
     }
-    
+
+    // called by options button, opens the options panel
+    public void onOptionsButton()
+    {
+        menuStateCoordinator.ShowOptions();
+    }
+
+    // called by back buttons, returns to previous menu screen
+    public void onBackButton()
+    {
+        menuStateCoordinator.Back();
+    }
+
+    // called by quit button, closes app in build
     public void onQuitButton()
     {
-        Application.Quit();
+        menuStateCoordinator.QuitGame();
+    }
+
+    // called by stage buttons, loads whatever scene name the button passes in
+    public void onSelectStageByName(string sceneName)
+    {
+        menuStateCoordinator.LoadStage(sceneName);
     }
 }
